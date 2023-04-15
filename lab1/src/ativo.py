e1d907b1215567e65b39e86e38597145d7bef89a
EduardoMeloC <33487458+EduardoMeloC@users.noreply.github.com>
2023-04-15 13:40:29 -0300

    Altera formatação do código

--- lab1/src/ativo.py
+++ lab1/src/ativo.py
@@ -1,28 +1,31 @@
-# Client 
+# Client
 
 import socket
 
 HOST = 'localhost'
 PORT = 5001
 
+
 def send_data(sock, data):
-  msg_to_server = data.encode('utf-8')
-  sock.send(msg_to_server)
+    msg_to_server = data.encode('utf-8')
+    sock.send(msg_to_server)
+
 
 def receive_data(sock):
-  msg_from_server = sock.recv(1024)
-  string_from_server = msg_from_server.decode('utf-8')
-  return string_from_server
+    msg_from_server = sock.recv(1024)
+    string_from_server = msg_from_server.decode('utf-8')
+    return string_from_server
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-  sock.connect((HOST, PORT)) 
+    sock.connect((HOST, PORT))
 
-  running = True
-  while running:
-    user_input = input()
-    if user_input == 'fim':
-      break
+    running = True
+    while running:
+        user_input = input()
+        if user_input == 'fim':
+            break
 
-    send_data(sock, user_input)
-    string_from_server = receive_data(sock)
-    print(string_from_server)
+        send_data(sock, user_input)
+        string_from_server = receive_data(sock)
+        print(string_from_server)
--- lab1/src/passivo.py
+++ lab1/src/passivo.py
@@ -5,23 +5,26 @@ import socket
 HOST = ''
 PORT = 5001
 
+
 def send_data(sock, data):
-  sock.send(data)
+    sock.send(data)
+
 
 def receive_data(sock):
-	data = sock.recv(1024)
-	return data
+    data = sock.recv(1024)
+    return data
+
 
 running = True
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-	sock.bind((HOST, PORT))
-	sock.listen(1) 
+    sock.bind((HOST, PORT))
+    sock.listen(1)
 
-	clientSock, address = sock.accept()
-	print(address)
+    clientSock, address = sock.accept()
+    print(address)
 
-	while running:
-		msg_from_client = receive_data(clientSock)
-		if not msg_from_client: 
-			break
-		send_data(clientSock, msg_from_client)
+    while running:
+        msg_from_client = receive_data(clientSock)
+        if not msg_from_client:
+            break
+        send_data(clientSock, msg_from_client)
