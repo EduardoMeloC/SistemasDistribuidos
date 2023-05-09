970c4cf32fb1f0f5e4f8b95c73d47eb0de6fd637
EduardoMeloC <33487458+EduardoMeloC@users.noreply.github.com>
2023-05-09 01:56:40 -0300

    lab2 - servidor com threads

--- lab2/solution/Servidor/Dominio/ServicoDicionario.py
+++ lab2/solution/Servidor/Dominio/ServicoDicionario.py
@@ -1,26 +1,40 @@
 from Dominio.IDicionario import IDicionario
 from Dominio.IRepositorio import IRepositorio
 
+from threading import Lock
+
 class ServicoDicionario(object):
     def __init__(self, dicionario: IDicionario, repositorio: IRepositorio):
         self.__dicionario = dicionario
         self.__repositorio = repositorio
+        self.__lock = Lock()
     
     def ler(self, chave):
-        return self.__dicionario.ler(chave)
+        self.__lock.acquire()
+        valor = self.__dicionario.ler(chave)
+        self.__lock.release()
+        return valor
 
     def escrever(self, chave, valor):
+        self.__lock.acquire()
         self.__dicionario.escrever(chave, valor)
+        self.__lock.release()
 
     def apagar(self, chave):
+        self.__lock.acquire()
         self.__dicionario.apagar(chave)
+        self.__lock.release()
 
     def salvar(self):
+        self.__lock.acquire()
         self.__repositorio.salvar(self.__dicionario)
+        self.__lock.release()
 
     def carregar(self):
+        self.__lock.acquire()
         dicionario_carregado = self.__repositorio.carregar()
         for key, value_list in dicionario_carregado.items():
             for value in value_list:
                 self.__dicionario.escrever(key, value)
+        self.__lock.release()
 
