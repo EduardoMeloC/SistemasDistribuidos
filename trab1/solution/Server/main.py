3207d82edbe526adec04ac6aa5f87128b7d8652c
EduardoMeloC <33487458+EduardoMeloC@users.noreply.github.com>
2023-06-25 04:51:09 -0300

    trab1 - início da implementação

--- trab1/solution/Server/main.py
+++ trab1/solution/Server/main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Callable, Optional, Tuple, TypeAlias
+from typing import Callable, Optional, Tuple, TypeAlias, List
 from dataclasses import dataclass
 
 import rpyc  # type: ignore
@@ -35,25 +35,49 @@ FnNotify: TypeAlias = Callable[[list[Tuple[Topic, Content]]], None]
 
 
 class BrokerService(rpyc.Service):  # type: ignore
+    def __init__(self):
+        self.__topicsfile = "topics.json"
+        self.users: dict[UserId, UserInfo] = {0: "admin"}
+        self.topics: dict[
+            Topic, List[Tuple[UserId, FnNotify]]
+        ] = self.load_topics_from_file()
+
+    def load_topics_from_file(self):
+        try:
+            with open(self.__topicsfile, "r") as topics_file:
+                topics = json.load(topics_file)
+                for topic_name, _ in topics.items():
+                    self.create_topic("admin", topic_name)
+        except FileNotFoundError:
+            with open(self.__dbfile, "w") as topics_file:
+                topics_file.write("{}")
+
     # Não é exposed porque só o "admin" tem acesso
     def create_topic(self, id: UserId, topicname: str) -> Topic:
-        assert False, "TO BE IMPLEMENTED"
+        if topicname not in self.topics:
+            self.topics[topicname] = ("admin", lambda: None)
+        return Topic(topicname)
 
     # Handshake
-
     def exposed_login(self, username: str) -> Optional[UserId]:
-        assert False, "TO BE IMPLEMENTED"
+        if username not in self.users.values():
+            next_userid = list(self.users.keys())[-1] + 1
+            self.users[next_userid] = username
+            return next_userid
+        else:
+            for id, name in self.users.items():
+                if name == username:
+                    return id
 
     # Query operations
-
     def exposed_get_user_info(self, id: UserId) -> UserInfo:
-        assert False, "TO BE IMPLEMENTED"
+        if id in self.users:
+            return self.users[id]
 
     def exposed_list_topics(self) -> list[Topic]:
-        assert False, "TO BE IMPLEMENTED"
+        return list(self.topics.keys())
 
     # Publisher operations
-
     def exposed_publish(self, id: UserId, topic: Topic, data: str) -> None:
         assert False, "TO BE IMPLEMENTED"
 
@@ -62,10 +86,15 @@ class BrokerService(rpyc.Service):  # type: ignore
     def exposed_subscribe_to(
         self, id: UserId, topic: Topic, callback: FnNotify
     ) -> Optional[FnNotify]:
-        assert False, "TO BE IMPLEMENTED"
+        if topic in self.topics:
+            self.topics[topic].append((id, callback))
+            return FnNotify
 
     def exposed_unsubscribe_to(self, id: UserId, topic: Topic) -> Optional[FnNotify]:
-        assert False, "TO BE IMPLEMENTED"
+        if topic in self.topics:
+            for subscriber in self.topics[topic]:
+                if subscriber[0] == id:
+                    pass  # Unsubscribe
 
     # Nota: A semântica do subscribe_all não foi definida, opções:
     # 0 - Jogar essa função fora (e unsubscribe_all)
